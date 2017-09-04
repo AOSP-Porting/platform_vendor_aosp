5d7bd2691ef18d586739af290138207fff636b0c
Simon Shields <simon@lineageos.org>
2017-09-04 21:11:16 +1000

    build: soong: put extra variables in the "Extra" struct
    Change-Id: I9ed55cea0c55f0078f8f97ad4037b16ed11c0156

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,5 +1,8 @@
+# Insert new variables inside the Extra structure
 extra_soong:
 	$(hide) mkdir -p $(dir $@)
 	$(hide) (\
 	echo '{'; \
+	echo '"Extra": {'; \
+	echo '},'; \
 	echo '') > $(SOONG_VARIABLES_TMP)
