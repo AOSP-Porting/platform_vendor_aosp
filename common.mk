43ed3c6bcf27136f66043209ddc4e52621fff66c
Alberto97 <albertop2197@gmail.com>
2017-10-21 20:52:04 +0200

    First commit

---
+++ common.mk
@@ -0,0 +1,96 @@
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+
+ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.clientidbase=android-google
+else
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.clientidbase=$(PRODUCT_GMS_CLIENTID_BASE)
+endif
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    keyguard.no_require_sim=true
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.selinux=1
+
+ifneq ($(TARGET_BUILD_VARIANT),user)
+# Thank you, please drive thru!
+PRODUCT_PROPERTY_OVERRIDES += persist.sys.dun.override=0
+endif
+
+ifneq ($(TARGET_BUILD_VARIANT),eng)
+# Enable ADB authentication
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.adb.secure=1
+endif
+
+ifeq ($(BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE),)
+  PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.device.cache_dir=/data/cache
+else
+  PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.device.cache_dir=/cache
+endif
+
+# APN list
+PRODUCT_COPY_FILES += \
+    vendor/aosp/apns-conf.xml:system/etc/apns-conf.xml
+
+# Backup Services whitelist
+PRODUCT_COPY_FILES += \
+    vendor/aosp/config/backup.xml:system/etc/sysconfig/backup.xml
+
+# Enable SIP+VoIP on all targets
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
+
+# Enable wireless Xbox 360 controller support
+PRODUCT_COPY_FILES += \
+    frameworks/base/data/keyboards/Vendor_045e_Product_028e.kl:system/usr/keylayout/Vendor_045e_Product_0719.kl
+
+# Optional packages
+PRODUCT_PACKAGES += \
+    libemoji \
+    LiveWallpapersPicker \
+    PhotoTable \
+    Terminal
+
+# Include explicitly to work around GMS issues
+PRODUCT_PACKAGES += \
+    libprotobuf-cpp-full \
+    librsjni
+
+# Exchange support
+PRODUCT_PACKAGES += \
+    Exchange2
+
+# Extra tools
+PRODUCT_PACKAGES += \
+    bzip2 \
+    curl \
+    gdbserver \
+    micro_bench \
+    mke2fs \
+    sqlite3 \
+    strace \
+    tune2fs
+
+# Openssh
+PRODUCT_PACKAGES += \
+    scp \
+    sftp \
+    ssh \
+    sshd \
+    sshd_config \
+    ssh-keygen \
+    start-ssh
+
+# Telephony packages
+PRODUCT_PACKAGES += \
+    Stk \
+
+# Storage manager
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.storage_manager.enabled=true
+
+DEVICE_PACKAGE_OVERLAYS += vendor/aosp/overlay
