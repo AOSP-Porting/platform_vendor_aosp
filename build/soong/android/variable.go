d183eca9e168ded012f2132cf98c74b0dd6a646a
Kevin F. Haggerty <haggertk@lineageos.org>
2018-08-13 03:52:42 -0600

    soong_config: Add TARGET_NEEDS_LEGACY_CAMERA_HAL1_DYN_NATIVE_HANDLE
    * A certain subset of legacy HAL1 camera devices need to use this
      specific port mode with this specific buffer type. See
      I15f8cbb3956773e2ba97032203f5e9254505d8ac for details.
    
    Change-Id: Ib35e14cb5cf80efc9e82a301b8e47680202f3905

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -3,6 +3,9 @@ type Product_variables struct {
 	Has_legacy_camera_hal1 struct {
 		Cflags []string
 	}
+	Needs_legacy_camera_hal1_dyn_native_handle struct {
+		Cppflags []string
+	}
 	Target_shim_libs struct {
 		Cppflags []string
 	}
@@ -10,5 +13,6 @@ type Product_variables struct {
 
 type ProductVariables struct {
 	Has_legacy_camera_hal1  *bool `json:",omitempty"`
+	Needs_legacy_camera_hal1_dyn_native_handle  *bool `json:",omitempty"`
 	Target_shim_libs  *string `json:",omitempty"`
 }
--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -5,6 +5,7 @@ extra_soong:
 	echo '{'; \
 	echo '"Extra": {'; \
 	echo '    "Has_legacy_camera_hal1": $(if $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)),true,false),'; \
+	echo '    "Needs_legacy_camera_hal1_dyn_native_handle": $(if $(filter true,$(TARGET_NEEDS_LEGACY_CAMERA_HAL1_DYN_NATIVE_HANDLE)),true,false),'; \
 	echo '    "Target_shim_libs": "$(subst $(space),:,$(TARGET_LD_SHIM_LIBS))"'; \
 	echo '},'; \
 	echo '') > $(SOONG_VARIABLES_TMP)
