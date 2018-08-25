872d529679ee5b1c79bb102c15473107be8e0f18
Rashed Abdel-Tawab <rashed@linux.com>
2018-08-25 11:10:25 -0700

    Move QC board variables earlier
    These variables are usually set in a device's BoardConfig, setting them
    in qcom_target is at the very end of the "configuration process" which
    results in them being unavailable to plenty of other configuration
    "things" (ex. soong namespaces or soong config). Move them to right
    after a device's BoardConfig has been found and loaded.
    
    Change-Id: Iddd731202d22ed3f8eb010197ce20d3c75a1f40a

--- config/BoardConfigExtra.mk
+++ config/BoardConfigExtra.mk
@@ -1,4 +1,3 @@
-# QCOM HW crypto
-ifeq ($(TARGET_HW_DISK_ENCRYPTION),true)
-    TARGET_CRYPTFS_HW_PATH ?= vendor/qcom/opensource/cryptfs_hw
-endif
+ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
+include vendor/aosp/config/BoardConfigQcom.mk
+endif 
