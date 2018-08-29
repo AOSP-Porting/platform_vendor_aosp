1b4a8bfed8e31e5b11faaadc58fb3b0eb61650da
Danny Baumann <dannybaumann@web.de>
2018-08-29 11:04:58 +0200

    soong_config: Allow process-specific override of target SDK version
    Change-Id: I775e852c17af12d2233e644e3db10c10bcb20d19

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -3,6 +3,9 @@ type Product_variables struct {
 	Has_legacy_camera_hal1 struct {
 		Cflags []string
 	}
+	Target_process_sdk_version_override struct {
+		Cppflags []string
+	}
 	Target_shim_libs struct {
 		Cppflags []string
 	}
@@ -10,5 +13,6 @@ type Product_variables struct {
 
 type ProductVariables struct {
 	Has_legacy_camera_hal1  *bool `json:",omitempty"`
+	Target_process_sdk_version_override *string `json:",omitempty"`
 	Target_shim_libs  *string `json:",omitempty"`
 }
